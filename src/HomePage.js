9cc29df1d7c8809a1bcca2bed6eee43443ba6a70
Jake M <jmorgenstern15@gmail.com>
2022-07-27 22:09:16 -0500

    Added home page for mobile and desktop

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,11 +1,84 @@
-const HomePage = () => {
+import jake from "./jake_photo.jpg";
+
+function HomePage() {
+    const isMobile = window.innerWidth <= 768;
+
+    return (
+        
+        isMobile ? <Mobile /> : <Desktop />
+    );
+};
+
+const Divider = () => <hr className="sidebar-hr pt-5" />;
+
+const Desktop = () => {
     return (
         <div className="content-container">
             <div className="text-white text-center font-bold text-7xl pt-4">
                 Jake Morgenstern
+                <div className="text-white font-thin text-center text-5xl pt-5">
+                    Game Developer and Student at Indiana University
+                    <div className="flex overflow-auto items-center pt-10">
+                        <div className="flex object-left pl-[200px] pr-20 pt-5">
+                            <img src={jake} alt="Jake Morgenstern headshot" width={1728} height={2304} />
+                        </div>
+                        <div className="text-lg font-bold text-center pr-[200px] pt-5">
+                            Hi, my name is Jake Morgenstern, and I'm a game development and computer science student at Indiana University
+                            Bloomington. I primarily use the Unity game engine, but I also have experience with Unreal Engine 5. Use the
+                            bar on the left to explore my projects, learn more about me, or get in contact. If you want to take a look
+                            at what I've done, click the button below to view my Github.
+
+                            <Divider />
+
+                            <button onClick={()=>{window.open(
+                                                    'https://www.github.com/toastyman231',
+                                                    '_blank' // <- This is what makes it open in a new window.
+                                                );}} 
+                                    className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                                        View my GitHub!</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
-};
+}
+
+const Mobile = () => {
+    return(
+        <div className="content-container h-screen overflow-y-scroll">
+            <div className="text-white text-center font-bold text-5xl pt-4">
+                Jake Morgenstern
+                <div className="text-white font-thin text-center text-xl pt-5">
+                    Game Developer and Student at Indiana University
+
+                    <div className="flex flex-col items-center">
+                        <div className="object-center pt-5">
+                            <img src={jake} alt="Jake Morgenstern headshot" width={window.innerWidth} height={window.innerHeight/2} />
+                        </div>
+
+                        <div className="text-lg font-bold text-center pl-5 pr-5 pt-5">
+                            Hi, my name is Jake Morgenstern, and I'm a game development and computer science student at Indiana University
+                            Bloomington. I primarily use the Unity game engine, but I also have experience with Unreal Engine 5. Use the
+                            bar on the left to explore my projects, learn more about me, or get in contact. If you want to take a look
+                            at what I've done, click the button below to view my Github.
+
+                            <Divider />
+
+                            <button onClick={()=>{window.open(
+                                            'https://www.github.com/toastyman231',
+                                            '_blank' // <- This is what makes it open in a new window.
+                                        );}} 
+                                    className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                                View my GitHub!</button>
+
+                            <Divider />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 export default HomePage;
